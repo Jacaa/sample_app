64c8310d7a0eeb2a0db1ec71027747d6b2d51590
Jacek <mikolajczak.jaca@gmail.com>
2017-03-22 23:53:42 +0100

    Finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,         "RoR Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | RoR Tutorial Sample App"
+    assert_equal full_title("Sign up"), "Sign up | RoR Tutorial Sample App"
+  end
+end
